4474225937fe5450108cc497b35aca7a6d4601ce
Jack Winslow <jackwinslow35@gmail.com>
2022-11-12 15:20:37 -0500

    Initial Commit

---
+++ Trainium/TrainiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainiumApp.swift
+//  Trainium
+//
+//  Created by Jack Winslow on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TrainiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
