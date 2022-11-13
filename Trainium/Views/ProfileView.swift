e664d685c019e5cc56402c902f62cad98d897a0d
Colin Arber <arberc@bc.edu>
2022-11-12 20:01:14 -0500

    Profile view with assets in proper locations (needs dynamic 'your recent workouts list')

--- Trainium/Views/ProfileView.swift
+++ Trainium/Views/ProfileView.swift
@@ -11,46 +11,44 @@ struct Profile: View {
     var body: some View {
         ZStack {
             VStack {
-                
+            
                 Image(systemName: "gear")
                     .resizable()
                     .frame(width:40, height: 40)
                     .opacity(0.5)
+                    .frame(maxWidth: .infinity, alignment:
+                            .trailing)
+                    .padding(.trailing, 30)
+                    .padding(.top, 0)
+                    
 
                 Image(systemName: "person.circle.fill")
                     .resizable()
                     .frame(width:100, height: 100)
                     .opacity(0.5)
                 
-                Text("<Your Name>")
+                Text("Joe Schmo")
+                    .foregroundColor(Color(0x424B54))
+                    .frame(maxWidth: .infinity, alignment:
+                            .center)
+                
+                Text("You are on a 15 day streak🔥")
                     .foregroundColor(Color(0x424B54))
                     .frame(maxWidth: .infinity, alignment: .center)
+
                 Spacer()
+                    .frame(height: 35)
                 
-                Text("Your stats")
+                Text("Your Recent Workouts")
                     .foregroundColor(Color(0x424B54))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.leading, 15)
+                
                 Spacer()
             }
+            
             .font(.system(size: 20))
             .fontWeight(.bold)
-            .padding(.top, 300)
-            
-            /*HStack {
-                Text("🔥 x day streak")
-                    .padding(10)
-                    .foregroundColor(Color(0xFCF7EE))
-                    .background(Color(0xFE2036).opacity(0.7)) // might take out opacity
-                    .cornerRadius(12)
-                    .padding(.leading, 20)
-                Text("🏋️ x PRs this month")
-                    .padding(10)
-                    .foregroundColor(Color(0xFCF7EE))
-                    .background(Color(0xFE2036).opacity(0.7)) // might take out opacity
-                    .cornerRadius(12)
-                    .padding(.leading, 20)
-            } */
             .frame(maxWidth: .infinity, alignment: .leading)
             .fontWeight(.medium)
     
