fdac049d6e922e082cc8bafdfd09d06e291b2c56
aidandaly20 <72171581+aidandaly20@users.noreply.github.com>
2022-11-13 03:07:01 -0500

    settings page

---
+++ Trainium/Views/Settings.swift
@@ -0,0 +1,125 @@
+//
+//  Settings.swift
+//  Trainium
+//
+//  Created by Aidan Daly on 11/13/22.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var username = ""
+    @State private var password = ""
+    var body: some View {
+        ZStack {
+            
+            Color("white").ignoresSafeArea()
+            VStack(spacing: 0) {
+                Text("Settings")
+                    .foregroundColor(Color("black"))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.system(size: 45))
+                    .padding()
+                
+                Text("Change Username:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .foregroundColor(Color("black"))
+                    .fontWeight(.medium)
+                    .font(.system(size: 25))
+                
+                TextField("", text: $username)
+                    .frame(width: 350, height: 35)
+                    .disableAutocorrection(true)
+                    .foregroundColor(Color(0xFE2036))
+                    .accentColor(Color(0xFE2036))
+                    .padding(.leading,12)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color(0x424B54), lineWidth: 1)
+                    )
+                
+                Text("Change First Name:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .foregroundColor(Color("black"))
+                    .fontWeight(.medium)
+                    .font(.system(size: 25))
+                
+                TextField("", text: $firstName)
+                    .frame(width: 350, height: 35)
+                    .disableAutocorrection(true)
+                    .foregroundColor(Color(0xFE2036))
+                    .accentColor(Color(0xFE2036))
+                    .padding(.leading,12)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color(0x424B54), lineWidth: 1)
+                    )
+                
+                
+                Text("Change Last Name:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .foregroundColor(Color("black"))
+                    .fontWeight(.medium)
+                    .font(.system(size: 25))
+                
+                TextField("", text: $lastName)
+                    .frame(width: 350, height: 35)
+                    .disableAutocorrection(true)
+                    .foregroundColor(Color(0xFE2036))
+                    .accentColor(Color(0xFE2036))
+                    .padding(.leading,12)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color(0x424B54), lineWidth: 1)
+                    )
+                
+                
+                Text("Password")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .foregroundColor(Color("black"))
+                    .fontWeight(.medium)
+                    .font(.system(size: 25))
+                
+                SecureField("", text: $password)
+                    .frame(width: 350, height: 35)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .foregroundColor(Color(0xFE2036))
+                    .accentColor(Color(0xFE2036))
+                    .padding(.leading,12)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color(0x424B54), lineWidth: 1)
+                    )
+                    .padding(.bottom, 40)
+                
+                
+                Button {
+                } label : {
+                    Text("Log Out")
+                        .foregroundColor(Color("red"))
+                        .font(.system(size: 25))
+                }
+                
+                
+                
+                
+                
+                
+            }
+        }
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
