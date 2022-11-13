aa3ea11c954ae406d71b67812caf4c8a76925af7
aidandaly20 <72171581+aidandaly20@users.noreply.github.com>
2022-11-13 02:41:20 -0500

    search isnt working

--- Trainium/Views/SearchView.swift
+++ Trainium/Views/SearchView.swift
@@ -12,13 +12,51 @@ struct SearchView: View {
     var body: some View {
         ZStack {
             
-            Color(0xFCF7EE).ignoresSafeArea()
+            Color("white").ignoresSafeArea()
             VStack(spacing: 0) {
                 NavigationView {
-                            Text("Searching for \(searchText)")
-                                .searchable(text: $searchText)
-                                .navigationTitle("Search For a Friend")
+                    Text("")
+                        .searchable(text: $searchText)
+                        .navigationTitle("Search For a Friend")
+                        .foregroundColor(Color("black"))
+                }
+                
+                Text("Friends")
+                    .font(.system(size: 30))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(0x424B54))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 10)
+                    .padding(.bottom)
+                
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12) {
+                        ForEach(0..<10) {_ in
+                            VStack {
+                                Button {
+                                    
+                                } label : {
+                                    Image(systemName: "person.circle.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height:75)
+                                        .foregroundColor(Color(0x424B54))
+                                }
+                                .buttonStyle(StaticButtonStyle())
+                                
+                                Text("Friend Name")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(Color(0x424B54))
+                            }
+                            .frame(width: 100)
                         }
+                    }
+                    .padding(.leading, 15)
+                }
+                .padding(.bottom, 25)
+                
+                Spacer()
             }
         }
     }
